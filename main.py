a20e5d227c08fb53b112e010c567a359feabce4a
rolongelves <rolongelves@gmail.com>
2025-05-27 09:09:11 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+
+import requests
+import feedparser
+import time
+from googletrans import Translator
+import os
+
+rss_url = 'https://cms.zerohedge.com/fullrss2.xml'
+bot_token = os.getenv("BOT_TOKEN")
+chat_id = os.getenv("CHAT_ID")
+
+translator = Translator()
+last_title = ""
+
+while True:
+    feed = feedparser.parse(rss_url)
+    if feed.entries:
+        latest_entry = feed.entries[0]
+        if latest_entry.title != last_title:
+            title_es = translator.translate(latest_entry.title, src='en', dest='es').text
+            description_es = translator.translate(latest_entry.summary, src='en', dest='es').text
+
+            message = f"📰 {title_es}\n\n{description_es}\n\n{latest_entry.link}"
+            send_url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
+            params = {'chat_id': chat_id, 'text': message}
+            requests.get(send_url, params=params)
+            last_title = latest_entry.title
+    time.sleep(10)
